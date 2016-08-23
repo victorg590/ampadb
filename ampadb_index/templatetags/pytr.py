15bcdb3173fb49b914b96e2047b90bf599408e8f
Victor Guardia <victorg590cole@gmail.com>
2016-08-23 20:07:06 +0200

    Afegit BS a Importar i Exportar

--- ampadb_index/templatetags/pytr.py
+++ ampadb_index/templatetags/pytr.py
@@ -4,16 +4,6 @@ from django.utils.html import mark_safe, format_html
 register = template.Library()
 
 @register.filter
-def pytr(value):
-    if value:
-        ret = '<span style="color:green">Sí</span>'
-    elif value is None:
-        ret = '-'
-    else:
-        ret = '<span style="color:red">No</span>'
-    return mark_safe(ret)
-
-@register.filter
 def pytricon(value):
     if value:
         color = '#008000'
@@ -27,3 +17,15 @@ def pytricon(value):
     return format_html(
         '<span class="glyphicon glyphicon-{}" style="color:{}"></span>',
         ret, color)
+
+@register.filter
+def pytr(value, icons=True):
+    if icons:
+        return pytricon(value)
+    if value:
+        ret = '<span style="color:green">Sí</span>'
+    elif value is None:
+        ret = '-'
+    else:
+        ret = '<span style="color:red">No</span>'
+    return mark_safe(ret)
