c8971b330c4f96c4e6259337ece8d5f02c016493
Victor Guardia <victorg590cole@gmail.com>
2016-12-15 18:17:07 +0100

    Afegit suport per a cerques AND i OR a Usuaris
    [Fixes #5]

---
+++ ampadb/datasearch.py
@@ -0,0 +1,59 @@
+import shlex
+from django.db.models.query_utils import Q
+
+_dummycomposer = lambda key, exact: Q()
+
+def parse_token(token, composer):
+    if token[0] == '"':
+        return composer(token[1:-1], False)
+    elif token[0] == "'":
+        return composer(token[1:-1], True)
+    elif '|' in token:
+        first, *other = token.split('|')
+        current = parse_token(first, composer)
+        for t in other:
+            current |= parse_token(t, composer)
+        return current
+    else:
+        return composer(token, False)
+
+def make_queryset_from_list(qlist):
+    if not qlist:
+        return Q()
+    current = qlist[0]
+    for q in qlist[1:]:
+        current &= q
+    return current
+
+def make_queryset_from_or_list(qlist):
+    if not qlist:
+        return Q()
+    current = qlist[0]
+    for q in qlist[1:]:
+        current |= q
+    return current
+
+def datasearch(params, composer=_dummycomposer):
+    """Torna un objecte Q segons un string de cerca.
+
+    ``params`` és l'string amb sintaxis de cerca.
+    ``composer`` és una funció que torna un objecte Q. Els seus arguments són:
+      * ``key``: una clau simple. S'hauria d'utilizar com
+        ``Q(camp__contains=key).``
+      * ``exact``: un booleà que indica si el valor ha de ser exacte
+    """
+    parser = shlex.shlex(params, posix=True)
+    parser.commenters = ''
+    parser.quotes = ''
+    parser.escapedquotes = '"\''
+    parser.whitespace_split = True
+    current = []
+    ors = []
+    for token in parser:
+        if token == '|':
+            ors.append(make_queryset_from_list(current))
+            current = []
+            continue
+        current.append(parse_token(token, composer))
+    ors.append(make_queryset_from_list(current))
+    return make_queryset_from_or_list(ors)
